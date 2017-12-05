7fd97345c8b8e94525cfea2a09ef7a00c187e715
Nelson Frew <nnfre1@student.monash.edu>
2017-12-05 11:55:04 +1100

    New repo

--- cheesecake.c
+++ cheesecake.c
@@ -12,7 +12,7 @@
 
 #include <math.h>
 #include <stdio.h>
-
+#include <string.h>
 
 /* This typename block is stolen from https://stackoverflow.com/questions/6280055/how-do-i-check-if-a-variable-is-of-a-certain-type-compare-two-types-in-c
 When I'm done I'll reduce the size of this block to just the necessary ones.
@@ -63,7 +63,7 @@ void DP_fill_in_general_cases(const int width,
                               const int problem_weights[]);
 int DP_find_best_solution(const int width,
                         const int n,
-                        int DP_table[][width],
+                        const int DP_table[][width],
                         const int capacity,
                         const int my_pinf);
 void DP_derive_solution_set(int n,
@@ -127,10 +127,12 @@ void DP(int problem_profits[],
                                        max_profit,
                                        2);
 
-  printf("p_upper_bound: %d \n", p_upper_bound);
+  //printf("p_upper_bound: %d \n", p_upper_bound);
 
   // Define DP table (n+1)*(nP)
   int DP_table[n+1][p_upper_bound];
+  // HMMMMM?
+  //memset(DP_table, 0, sizeof(DP_table[0][0]) * (n+1) * p_upper_bound);
 
   // Compute base cases
   DP_fill_in_base_cases(p_upper_bound,
@@ -145,18 +147,18 @@ void DP(int problem_profits[],
                            DP_table,
                            problem_profits,
                            problem_weights);
-
+  int my_pinf = derive_pinf(problem_weights, n);
   /* Debugging section: print to ensure that
      the general cases were filled in...     */
   printf("DEBUG: DP general case print-out...\n");
   for (int k = 0; k < p_upper_bound; k++){
     printf("%2d-",k);
   }printf("\n");
-  int my_pinf = derive_pinf(problem_weights, n);
+  
   for (int i = 0; i < n+1; i++){
     for (int j = 0; j < p_upper_bound; j++){
-      if(DP_table[i][j] == my_pinf) printf("%2s "," ∞");
-      else printf("%2d ", DP_table[i][j]);
+      if(DP_table[i][j] == my_pinf) printf("%2s "," ∞"); /**/
+      else printf("%2d ", DP_table[i][j]);/**/
     }printf("\n");
   }
   /* End debugging section */
@@ -167,7 +169,7 @@ void DP(int problem_profits[],
                                 DP_table,
                                 capacity,
                                 my_pinf);
-  printf("DEBUG: p=%d\n ",p);
+  //printf("DEBUG: p=%d\n ",p);
 
   // Derive S from the table
   DP_derive_solution_set(n+1,
@@ -287,7 +289,6 @@ void DP_fill_in_base_cases(const int width,
   */
   
   int my_pinf = derive_pinf(problem_weights, n);
-  printf("my_pinf: %d\n", my_pinf);
   // Go over the first column with 0's
   for(int i = 0; i < n; i++){
      DP_table[i][0] = 0; 
@@ -346,7 +347,8 @@ void DP_fill_in_general_cases(const int width,
   }
 }
 
-int derive_pinf(const int problem_weights[], const int n){
+int derive_pinf(const int problem_weights[], 
+                const int n){
   /*
     Description:
       Sums up all the item weights to obtain a trivial upper bound on the weight
@@ -366,15 +368,15 @@ int derive_pinf(const int problem_weights[], const int n){
 }
 
 int DP_find_best_solution(const int width,
-                        const int n,
-                        int DP_table[][width],
-                        const int capacity,
-                        const int my_pinf){
+                          const int n,
+                          const int DP_table[][width],
+                          const int capacity,
+                          const int my_pinf){
   /*
     Description:
       Given a completed DP_table, find max(p | A(n,p) <= Capacity)
     Inputs:
-      width - the width of the table
+      width - the width of the tables
       n - the height of the table (also the number of items)
       DP_table - the dynamic programming 2d array
       problem_profits - the input problem's associated item profits
@@ -392,10 +394,11 @@ int DP_find_best_solution(const int width,
   */
 
   int p= -1;
-  for(int i = width-1; i>=0; i--){
-    if (DP_table[n-1][i] != my_pinf){
-      if (DP_table[n-1][i] <= capacity){
-        printf("Oh boy! DP_table[n][i]: %d, n-1: %d, i: %d\n", DP_table[n-1][i], n-1, i);
+  for(int i = width-1; i>=0; i--){/*  */
+    if (DP_table[n-1][i] != my_pinf){/*  */
+      //printf("DP_table[%d][%d]: %d\n", n-1, i, DP_table[n-1][i]);
+      if (DP_table[n-1][i] <= capacity){/*  */
+        printf("Oh boy! DP_table[n-1][i]: %d, n-1: %d, i: %d\n", DP_table[n-1][i], n-1, i);
         p = i;
         break;
       } 
@@ -429,9 +432,10 @@ void DP_derive_solution_set(int n,
 
   int s_index = 0;
   while (p > 0){
-    if (DP_table[n-1][p] > DP_table[n][p]){
+    if (DP_table[n-2][p] > DP_table[n-1][p]){
       // then we had item n and the solution at A[n-1][p-profit(n)]
-      solution[s_index] = n;
+      printf("DP_table[n-2][p]: %d, DP_table[n-1][p]: %d, p: %d, n: %d\n", DP_table[n-2][p], DP_table[n-1][p], p, n);
+      solution[s_index] = (n-1);
       s_index += 1;
       n -= 1;
       p -= problem_profits[n-1]; // remember table n corresponds to profit n-1
